d9bd7ed9606f31746d89dc007cc00616e767d033
IgnacioRR23 <i.rodriguezrabal@gmail.com>
2023-03-11 02:30:12 +0100

    Creación atributos y Enum

---
+++ src/Limonada.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.time.LocalDate;
+
+public class Limonada {
+	
+	//Atributos
+	private LocalDate fecha;
+	private LugarVenta lugar;
+	private Integer limonesVendidos;
+	private Integer naranjasVendidas;
+	private Integer temperaturaF;
+	private Integer temperaturaC;
+	private Float folletosSuficientes;
+	private Double precio;
+
+}
