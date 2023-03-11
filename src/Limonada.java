275012ad24616c6d7ea314dc1f4e48308b65be37
IgnacioRR23 <i.rodriguezrabal@gmail.com>
2023-03-11 03:25:34 +0100

    Constructor 1

--- src/Limonada.java
+++ src/Limonada.java
@@ -6,6 +6,7 @@ public class Limonada {
 	
 	//Atributos
 	private LocalDate fecha;
+	private String ciudad;
 	private LugarVenta lugar;
 	private Integer limonesVendidos;
 	private Integer naranjasVendidas;
@@ -13,5 +14,17 @@ public class Limonada {
 	private Integer temperaturaC;
 	private Float folletosSuficientes;
 	private Double precio;
+	
+	//Constructores
+	public Limonada (LocalDate fecha, String ciudad, Integer limonesVendidos, Integer naranjasVendidas, Integer temperaturaF, Float folletosSuficientes, Double precio) {
+		this.fecha = fecha;
+		this.ciudad = ciudad;
+		this.limonesVendidos = limonesVendidos;
+		this.naranjasVendidas = naranjasVendidas;
+		this.temperaturaF = temperaturaF;
+		this.folletosSuficientes = folletosSuficientes;
+		this.precio = precio;
+		lugar = null;	
+	}
 
 }
